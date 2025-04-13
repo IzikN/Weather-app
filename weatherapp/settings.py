1a581237caf68736720b2ce42e3232e595d7aa8c
Isaac Nev <143568449+IzikN@users.noreply.github.com>
2025-04-13 15:57:43 +0100

    Update settings.py

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -115,8 +115,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+import os
+from whitenoise.middleware import WhiteNoiseMiddleware
 
-STATIC_URL = 'static/'
+# Middleware
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
